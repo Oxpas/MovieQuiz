73a68a1719f29550579ad75e30f288587169aa2d
Nikolay_Zamaraev <shisuyauchiha@gmail.com>
2025-06-27 23:23:00 +0700

    Сделано задание 6 спринта

--- MovieQuiz/QuestionFactory.swift
+++ MovieQuiz/QuestionFactory.swift
@@ -63,44 +63,48 @@ class QuestionFactory: QuestionFactoryProtocol {
     
     func requestNextQuestion() {
         DispatchQueue.global().async { [weak self] in
-            guard let self = self, let movie = self.movies.randomElement() else { return }
+            guard let self = self else { return }
+            let index = (0..<self.movies.count).randomElement() ?? 0
             
-            // Асинхронная загрузка изображения
-            URLSession.shared.dataTask(with: movie.image) { data, _, error in
-                if let error = error {
-                    DispatchQueue.main.async {
-                        self.delegate?.didFailToLoadData(with: error)
-                    }
-                    return
-                }
-                
-                guard let data = data else { return }
-                let rating = Float(movie.rating) ?? 0
-                let question = QuizQuestion(
-                    image: data,
-                    text: "Рейтинг этого фильма больше, чем 7?",
-                    correctAnswer: rating > 7
-                )
-                
-                DispatchQueue.main.async {
-                    self.delegate?.didRecieveNextQuestion(question: question)
-                }
-            }.resume()
+            guard let movie = self.movies[safe: index] else { return }
+            
+            var imageData = Data()
+            
+            do {
+                imageData = try Data(contentsOf: movie.resizedImageURL)
+            } catch {
+                print("Failed to load image")
+            }
+            
+            let rating = Float(movie.rating) ?? 0
+            
+            let text = "Рейтинг этого фильма больше чем 7?"
+            let correctAnswer = rating > 7
+            
+            let question = QuizQuestion(image: imageData,
+                                        text: text,
+                                        correctAnswer: correctAnswer)
+            
+            DispatchQueue.main.async { [weak self] in
+                guard let self else { return }
+                self.delegate?.didRecieveNextQuestion(question: question)
+            }
         }
     }
-    
-    func loadData() {
-        moviesLoader.loadMovies { [weak self] result in
-            DispatchQueue.main.async {
-                guard let self = self else { return }
-                switch result {
-                case .success(let mostPopularMovies):
-                    self.movies = mostPopularMovies.items
-                    self.delegate?.didLoadDataFromServer()
-                case .failure(let error):
-                    self.delegate?.didFailToLoadData(with: error)
+        
+        func loadData() {
+            moviesLoader.loadMovies { [weak self] result in
+                DispatchQueue.main.async {
+                    guard let self = self else { return }
+                    switch result {
+                    case .success(let mostPopularMovies):
+                        self.movies = mostPopularMovies.items
+                        self.delegate?.didLoadDataFromServer()
+                    case .failure(let error):
+                        self.delegate?.didFailToLoadData(with: error)
+                    }
                 }
             }
         }
     }
-}
+
