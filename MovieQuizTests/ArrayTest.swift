b28d6f523a0b4df02001740c5675b29cff8483b6
OxpaS <shisuyauchiha@gmail.com>
2026-01-18 18:12:36 +0700

    Added new features

--- MovieQuizTests/ArrayTest.swift
+++ MovieQuizTests/ArrayTest.swift
@@ -1,5 +1,5 @@
-import XCTest // не забывайте импортировать фреймворк для тестирования
-@testable import MovieQuiz // импортируем наше приложение для тестирования
+import XCTest
+@testable import MovieQuiz
 
 final class ArrayTests: XCTestCase {
     func testGetValueInRange() throws { // тест на успешное взятие элемента по индексу
